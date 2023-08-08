742837d33d18d57c776ea3e21f875d3abc37a53c
Zwivhuya <zwivhuyamuthathi@gmail.com>
2023-08-08 15:57:11 -0700

    If and elif python statements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # 8-uppercase.py
 
+
 def uppercase(str):
-"""Print string in uppercase."""
+"""Prints string in uppercase."""
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            c = chr(ord(c) - ord('a') + ord('A'))
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
         print("{}".format(c), end="")
     print("")
