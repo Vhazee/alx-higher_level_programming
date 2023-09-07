83320841976490b1e87c97ea01ecc35a4254d031
Zwivhuya <zwivhuyamuthathi@gmail.com>
2023-09-07 13:31:16 -0700

    Python test driven development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """ print my first and last name """
+    str_error = "first_name must be a string or last_name must be a string"
+    if type(first_name) is not str or type(last_name) is not str:
+        raise TypeError(str_error)
+    print("My name is", first_name, last_name)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """ print a square """
+    int_error = "size must be an integer"
+    neg_error = "size must be >= 0"
+    if type(size) is not int:
+        raise TypeError(int_error)
+    if size < 0:
+        raise ValueError(neg_error)
+    for i in range(size):
+        print("#" * size, end="")
+        print()
